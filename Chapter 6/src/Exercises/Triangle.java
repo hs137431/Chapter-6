fa4da33bc009676f2d3fa7c9ecee706d12046e4c
hs137431 <31348463+hs137431@users.noreply.github.com>
2017-09-29 10:05:00 -0500

    update

--- Chapter 6/src/Exercises/Triangle.java
+++ Chapter 6/src/Exercises/Triangle.java
@@ -6,14 +6,62 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a, b;
-		double c;
-		System.out.println("We are going to solve the slope side of a right triangle!");
+		double a = 0, b = 0;
+		double c = 0;
+		int choiceYes = 1;
+		String firstChoice;
 		Scanner input = new Scanner(System.in);
-		System.out.print("What will the first side of the triangle be?");
-		a = input.nextInt();
-		System.out.print("What will the second side of the triangle be?");
-		b = input.nextInt();
+		while (choiceYes == 1) {
+			System.out.println("This will help you solve sides of a right triangle using the pythagorean +"
+					+ "theorem! Note do not have Square numbers input");
+			
+		
+				System.out.println("Which side do you want to find a, b, or c >> ");
+		firstChoice = input.nextLine();
+		if(firstChoice.equalsIgnoreCase("a")) {
+			System.out.print("Please enter the value of B >> ");
+			b = input.nextDouble();
+			System.out.print("Please enter the value of C >> ");
+			c= input.nextDouble();
+			c = c * c;
+			b = b * b;
+		}
+		if(firstChoice.equalsIgnoreCase("b")) {
+			System.out.print("Please enter the value of C >> ");
+			c = input.nextDouble();
+			System.out.print("Please enter the value of A >> ");
+			a = input.nextDouble();
+			c = c * c;
+			a = a * a;
+		}
+		if(firstChoice.equalsIgnoreCase("c")) {
+			System.out.print("Please enter the value of B >> ");
+			b = input.nextDouble();
+			System.out.print("Please enter the value of A >> ");
+			a = input.nextDouble();
+			a = a * a;
+			b = b * b;
+		}
+		if (a == 0) {
+			a = Math.sqrt(c) - Math.sqrt(b);
+			System.out.println("Your answer for the missing side is " + a);
+			System.out.println("Do you want to do another triangle?");
+			choiceYes = input.nextInt();
+		}
+		if (b == 0) {
+			b = Math.sqrt(c) - Math.sqrt(a);
+			System.out.println("Your answer for the missing side is " + b);
+			System.out.println("Do you want to do another triangle?");
+			choiceYes = input.nextInt();
+		}
+		if (c == 0) {
+			c = Math.sqrt(a) + Math.sqrt(b);
+			System.out.println("Your answer for the missing side is " + c);
+			System.out.println("Do you want to do another triangle?");
+			choiceYes = input.nextInt();
+		}
+		}
+		input.close();
 	}
 
 }
